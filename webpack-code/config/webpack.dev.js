1e3a0534d9499e93066f428c79d76eb536fb9c85
252601153 <48664337+252601153@users.noreply.github.com>
2023-01-01 11:58:55 +0800

    hmr

--- webpack-code/config/webpack.dev.js
+++ webpack-code/config/webpack.dev.js
@@ -112,7 +112,8 @@ module.exports = {
     devServer: {
         host: 'localhost',
         port: '3000',
-        open: true, //是否自动打开浏览器
+        open: true, //是否自动打开浏览器,
+        hot: true, //默认值，开启热模块替换
     },
 
     //mode
