1bcaa0bc07603bc2ee881495fe263b0701085f64
252601153 <48664337+252601153@users.noreply.github.com>
2023-01-02 18:54:16 +0800

    js split

---
+++ code-split/demo1/webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path')
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+
+module.exports = {
+    //多入口文件
+    entry: {
+        app: './src/app.js',
+        main: './src/main.js',
+    },
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "[name].js", //webpack命名方式，[name]以文件名自己命名
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, "public/index.html"),
+        })
+    ],
+    mode: "production",
+}
