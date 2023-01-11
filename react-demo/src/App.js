9fd1ef18cccd70eef99e1a255e6a3e0c51b1f221
jakezhang <252601153@qq.com>
2023-01-11 14:40:18 +0800

    use reducer

--- react-demo/src/App.js
+++ react-demo/src/App.js
@@ -1,11 +1,18 @@
 import EffectHook from "./components/EffectHook";
 import Example from "./components/StateHook";
+import { Color } from "./components/use-reducer/color";
+import ShowArea from "./components/use-reducer/ShowArea";
+import Buttons from "./components/use-reducer/Buttons";
 
 function App() {
   return (
     <div className="App">
-      <Example />
-      <EffectHook />
+      {/* <Example />
+      <EffectHook /> */}
+      <Color>
+        <ShowArea />
+        <Buttons />
+      </Color>
     </div>
   );
 }
---
+++ react-demo/src/components/use-reducer/ShowArea.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { useContext } from 'react';
+import { ColorContext } from './color';
+
+const ShowArea = (props) => {
+  const { color } = useContext(ColorContext);
+  return <div style={{ color: color }}>字体颜色展示为{color}</div>;
+}
+
+export default ShowArea
